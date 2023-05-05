5ecf944c4a6f7709a0514783c9176ed877010dd5
sedat <somamebarge@gmail.com>
2023-05-05 12:36:57 +0200

    Methods

---
+++ src/main/java/methods/Methods.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class Methods { static int toplama(int a, int b) {
+
+        return a + b;
+    }
+        public static void main(String[] args) {
+            int sonuc = toplama(5, 2);
+            System.out.println(sonuc);
+        }
+    }
+
+
