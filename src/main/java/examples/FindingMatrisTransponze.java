3560de6abd803ad0b4ab386c47c4a0b7ad71bde3
sedat <somamebarge@gmail.com>
2023-05-11 00:34:15 +0200

    FindingMatrisTransponze.java

---
+++ src/main/java/examples/FindingMatrisTransponze.java
@@ -0,0 +1,44 @@
+package examples;
+import java.util.Scanner;
+
+
+public class FindingMatrisTransponze {
+
+
+           public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Matrisin satır sayısı: ");
+            int rows = scanner.nextInt();
+            System.out.print("Matrisin sütun sayısı: ");
+            int columns = scanner.nextInt();
+
+            int[][] matrix = new int[rows][columns];
+
+            System.out.println("Matrisin elemanlarını giriniz:");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    System.out.print((i + 1) + ". satır, " + (j + 1) + ". sütun: ");
+                    matrix[i][j] = scanner.nextInt();
+                }
+            }
+
+            int[][] transposedMatrix = new int[columns][rows];
+
+            for (int i = 0; i < columns; i++) {
+                for (int j = 0; j < rows; j++) {
+                    transposedMatrix[i][j] = matrix[j][i];
+                }
+            }
+
+            System.out.println("Transpoze:");
+            for (int i = 0; i < columns; i++) {
+                for (int j = 0; j < rows; j++) {
+                    System.out.print(transposedMatrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
