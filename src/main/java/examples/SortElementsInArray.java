48eb69a93c623f5dc3807c0548480ce9c07bd750
sedat <somamebarge@gmail.com>
2023-05-10 23:58:05 +0200

    SortElementsInArray.javantsInArray

---
+++ src/main/java/examples/SortElementsInArray.java
@@ -0,0 +1,28 @@
+package examples;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortElementsInArray {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        //Dizi boyutunu alıyoruz ve dizinin index değeri yerine yerleştiriyoruz.
+        System.out.print("Dizi Boyutunu Giriniz : ");
+        int indexArray = input.nextInt();
+        int[] numbers = new int[indexArray];
+
+        //Dizi elemanlarını döngü ile kullanıcıdan alıyoruz.
+        System.out.println("Dizinin Elemanlarını Giriniz : ");
+        for(int i=0; i < numbers.length; i++){
+            System.out.print((i+1) + ". Elemanı Gir : ");
+            numbers[i] = input.nextInt();
+        }
+        //Dizi hafızaya kaydedildiği için döngü dışında istediğimiz gibi diziyi ekrana bastırabiliyoruz.
+        System.out.println("Dizi : " + Arrays.toString(numbers));
+        Arrays.sort(numbers);
+        System.out.println("Sıralı : " + Arrays.toString(numbers));
+    }
+}
+
