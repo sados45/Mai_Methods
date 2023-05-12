4b7789f81044682ce03771a8ae33350af5ecb283
sedat <somamebarge@gmail.com>
2023-05-12 13:56:00 +0200

    SelectionSortProject

--- src/main/java/AlgortihmExample.java
+++ src/main/java/AlgortihmExample.java
@@ -1,6 +1,42 @@
 public class AlgortihmExample {
 
     public static void main(String[] args) {
-        
+
+
+    [22,27,16,2,18,6] -> Insertion Sort
+
+        Yukarı verilen dizinin sort türüne göre aşamalarını yazınız.
+
+        Big-O gösterimini yazınız.
+
+        Time Complexity: Dizi sıralandıktan sonra 18 sayısı aşağıdaki case'lerden hangisinin kapsamına girer? Yazınız
+
+        Average case: Aradığımız sayının ortada olması
+        Worst case: Aradığımız sayının sonda olması
+        Best case: Aradığımız sayının dizinin en başında olması.
+.
+
+
+
+[7,3,5,8,2,9,4,15,6] dizisinin Selection Sort'a göre ilk 4 adımını yazınız.
+
+        ChatGPT
+                [22, 27, 16, 2, 18, 6] dizisinin Insertion Sort'a göre aşamaları:
+
+        Adım: [22, 27, 16, 2, 18, 6]
+        Adım: [16, 22, 27, 2, 18, 6]
+        Adım: [2, 16, 22, 27, 18, 6]
+        Adım: [2, 16, 18, 22, 27, 6]
+        Adım: [2, 6, 16, 18, 22, 27]
+        Big-O gösterimi: O(n^2)
+
+        Time Complexity: Dizi sıralandıktan sonra 18 sayısı Worst case'a girer.
+
+                [7, 3, 5, 8, 2, 9, 4, 15, 6] dizisinin Selection Sort'a göre ilk 4 adımı:
+
+        Adım: [2, 3, 5, 8, 7, 9, 4, 15, 6]
+        Adım: [2, 3, 5, 8, 7, 9, 4, 15, 6]
+        Adım: [2, 3, 4, 8, 7, 9, 5, 15, 6]
+        Adım: [2, 3, 4, 5, 7, 9, 8, 15, 6]
     }
 }
